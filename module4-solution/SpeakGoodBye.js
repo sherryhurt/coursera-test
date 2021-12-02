c28544d0f783d17bc5520f0b5b35677948c94171
sherryhurt <94340605+sherryhurt@users.noreply.github.com>
2021-12-02 00:17:32 -0800

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -25,11 +25,10 @@
 // xxxx.xxxx = byeSpeaker;
  
 (function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window)
