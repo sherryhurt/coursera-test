c3fd1cb2a28a11ba8de69a362525430baa8b8c66
sherryhurt <94340605+sherryhurt@users.noreply.github.com>
2021-12-02 00:20:00 -0800

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -60,24 +60,19 @@ WARNING!!! WARNING!!!
  // } else {
  //   helloSpeaker.speak(names[i]);
   }
-}
+// }
 
-})();
+// })();
 
 
-(function () {
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
+(function() {
+    var names = ["Kevin", "Jenil", "Jay", "Abhi", "Parth", "Large", "Pavan", "Biren", "Jim","Jit"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
